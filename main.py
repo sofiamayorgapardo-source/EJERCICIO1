0ca6c53b302d11c216f190fc75d959689b3d9dd2
davidsaavdra castro09-beep <davidsaavedracastro09@gmail.com>
2026-05-12 17:04:51 -0500

    Implementacion de bucles for

--- main.py
+++ main.py
@@ -2,3 +2,7 @@ input("Bienvenidos al programa para saber tus nota Sa continuacion sigue los pas
 # Menú de opciones
 print("1. Ingresar varios estudiantes")
 print("2. Ingresar un solo estudiante")
+# Proceso para ingresar estudiantes
+for e in range(cantidad):
+
+    print(f" Estudiante #{e + 1}")
